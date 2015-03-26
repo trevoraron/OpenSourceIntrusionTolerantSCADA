28791a91a0c5b53e971e15f379d3b14b90cfcc0b
TrevorAron <taron1@jhu.edu>
2015-03-26 00:03:46 -0400

    SCADA example w/o graphing and modbus

---
+++ Demo/mask1_slots.h
@@ -0,0 +1,196 @@
+//###############################################################
+//# mask1_slots.h for ProcessViewServer created: Sun Mar 22 23:52:04 2015
+//# please fill out these slots
+//# here you find all possible events
+//# Yours: Lehrig Software Engineering
+//###############################################################
+
+// todo: uncomment me if you want to use this data aquisiton
+// also uncomment this classes in main.cpp and pvapp.h
+// also remember to uncomment rllib in the project file
+//extern rlModbusClient     modbus;
+//extern rlSiemensTCPClient siemensTCP;
+//extern rlPPIClient        ppi;
+#include <vector>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+typedef struct // (todo: define your data structure here)
+{
+  vector<double> data;
+  int amp;
+  //used for this demo
+  int time;
+}
+DATA;
+
+static int slotInit(PARAM *p, DATA *d)
+{
+  if(p == NULL || d == NULL) return -1;
+  d->amp = 1;
+  for(int i = 0; i < 200; i++){
+    d->data.push_back(sin(i/200.0) * amp);
+  }
+  d->time = 199;
+  return 0;
+}
+
+static int slotNullEvent(PARAM *p, DATA *d)
+{
+  if(p == NULL || d == NULL) return -1;
+  //TODO:replace with polling from modbus
+  double temp = sin((++d->time) / 200.0) * d->amp;
+  d->data.push_back(temp);
+  d->data.erase(d->data.begin());
+
+  
+  return 0;
+}
+
+static int slotButtonEvent(PARAM *p, int id, DATA *d)
+{
+  if(p == NULL || id == 0 || d == NULL) return -1;
+  return 0;
+}
+
+static int slotButtonPressedEvent(PARAM *p, int id, DATA *d)
+{
+  if(p == NULL || id == 0 || d == NULL) return -1;
+  return 0;
+}
+
+static int slotButtonReleasedEvent(PARAM *p, int id, DATA *d)
+{
+  if(p == NULL || id == 0 || d == NULL) return -1;
+  return 0;
+}
+
+
+static int slotTextEvent(PARAM *p, int id, DATA *d, const char *text)
+{
+  if(p == NULL || id == 0 || d == NULL || text == NULL) return -1;
+  try {
+    d->amp = atoi(text);
+  }
+  catch (int e) {
+    cout << "String Parsing exception \n";
+  }
+  return 0;
+}
+
+static int slotSliderEvent(PARAM *p, int id, DATA *d, int val)
+{
+  if(p == NULL || id == 0 || d == NULL || val < -1000) return -1;
+  return 0;
+}
+
+static int slotCheckboxEvent(PARAM *p, int id, DATA *d, const char *text)
+{
+  if(p == NULL || id == 0 || d == NULL || text == NULL) return -1;
+  return 0;
+}
+
+static int slotRadioButtonEvent(PARAM *p, int id, DATA *d, const char *text)
+{
+  if(p == NULL || id == 0 || d == NULL || text == NULL) return -1;
+  return 0;
+}
+
+static int slotGlInitializeEvent(PARAM *p, int id, DATA *d)
+{
+  if(p == NULL || id == 0 || d == NULL) return -1;
+  return 0;
+}
+
+static int slotGlPaintEvent(PARAM *p, int id, DATA *d)
+{
+  if(p == NULL || id == 0 || d == NULL) return -1;
+  return 0;
+}
+
+static int slotGlResizeEvent(PARAM *p, int id, DATA *d, int width, int height)
+{
+  if(p == NULL || id == 0 || d == NULL || width < 0 || height < 0) return -1;
+  return 0;
+}
+
+static int slotGlIdleEvent(PARAM *p, int id, DATA *d)
+{
+  if(p == NULL || id == 0 || d == NULL) return -1;
+  return 0;
+}
+
+static int slotTabEvent(PARAM *p, int id, DATA *d, int val)
+{
+  if(p == NULL || id == 0 || d == NULL || val < -1000) return -1;
+  return 0;
+}
+
+static int slotTableTextEvent(PARAM *p, int id, DATA *d, int x, int y, const char *text)
+{
+  if(p == NULL || id == 0 || d == NULL || x < -1000 || y < -1000 || text == NULL) return -1;
+  return 0;
+}
+
+static int slotTableClickedEvent(PARAM *p, int id, DATA *d, int x, int y, int button)
+{
+  if(p == NULL || id == 0 || d == NULL || x < -1000 || y < -1000 || button < 0) return -1;
+  return 0;
+}
+
+static int slotSelectionEvent(PARAM *p, int id, DATA *d, int val, const char *text)
+{
+  if(p == NULL || id == 0 || d == NULL || val < -1000 || text == NULL) return -1;
+  return 0;
+}
+
+static int slotClipboardEvent(PARAM *p, int id, DATA *d, int val)
+{
+  if(p == NULL || id == 0 || d == NULL || val < -1000) return -1;
+  return 0;
+}
+
+static int slotRightMouseEvent(PARAM *p, int id, DATA *d, const char *text)
+{
+  if(p == NULL || id == 0 || d == NULL || text == NULL) return -1;
+  //pvPopupMenu(p,-1,"Menu1,Menu2,,Menu3");
+  return 0;
+}
+
+static int slotKeyboardEvent(PARAM *p, int id, DATA *d, int val, int modifier)
+{
+  if(p == NULL || id == 0 || d == NULL || val < -1000 || modifier < -1000) return -1;
+  return 0;
+}
+
+static int slotMouseMovedEvent(PARAM *p, int id, DATA *d, float x, float y)
+{
+  if(p == NULL || id == 0 || d == NULL || x < -1000 || y < -1000) return -1;
+  return 0;
+}
+
+static int slotMousePressedEvent(PARAM *p, int id, DATA *d, float x, float y)
+{
+  if(p == NULL || id == 0 || d == NULL || x < -1000 || y < -1000) return -1;
+  return 0;
+}
+
+static int slotMouseReleasedEvent(PARAM *p, int id, DATA *d, float x, float y)
+{
+  if(p == NULL || id == 0 || d == NULL || x < -1000 || y < -1000) return -1;
+  return 0;
+}
+
+static int slotMouseOverEvent(PARAM *p, int id, DATA *d, int enter)
+{
+  if(p == NULL || id == 0 || d == NULL || enter < -1000) return -1;
+  return 0;
+}
+
+static int slotUserEvent(PARAM *p, int id, DATA *d, const char *text)
+{
+  if(p == NULL || id == 0 || d == NULL || text == NULL) return -1;
+  return 0;
+}
