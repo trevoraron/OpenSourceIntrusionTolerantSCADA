929485800b2fdbf6ecee0873f27df02cfcb14822
jayawrr <jcharl13@jhu.edu>
2015-03-31 13:34:27 -0400

    Update script_writer.py
    Added functionality to receive write values and update amplitude for demo

--- modbus_tcp_sim/script_writer.py
+++ modbus_tcp_sim/script_writer.py
@@ -5,7 +5,8 @@ import time
 import sys
 import fileinput
 
-v = [ 0x12, 0x34, 0x56, 0x78, 0x9A, 0xbc, 0xab, 0xcd, 0xef ]
+read = [ 0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xab, 0xcd, 0xef ]
+write = [ 0x00, 0x00, 0x00, 0x00, 0xff, 0x00, 0xff, 0x00 ]
 s = [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
 
 # Default amplitude of sine wave
@@ -16,18 +17,32 @@ else:
 
 while True:
 #for x in range(0,1):
-    # Calculate sine values
-    for i in xrange(0, len(v)):
-        s[i] = v[i] % amplitude
-        v[i] += 1
+    # Calculate function values
+    for i in xrange(0, len(read)):
+        if amplitude:
+            s[i] = read[i] % amplitude
+            read[i] += 1
 
     # Modify data lines of ini file
     for line in fileinput.input( "mtss.ini", inplace=True ):
+        # Find & modify read values
         if "read0x00000" in line:
             print "read0x00000=%02x %02x %02x %02x %02x %02x" % (s[0], s[1], s[2], s[3], s[4], s[5])
         elif "read0x04000" in line:
             print "read0x04000=%02x %02x %02x" % (s[6], s[7], s[8])
         else:
+            # Find and get write values
+            if "write0x" in line:
+                ptr = line.index( "=" )
+                index = int( line[ptr-1] ) * 2
+                vals = line[ptr+1:].split()
+                write[index] = int( vals[0], 16 )
+                write[index+1] = int( vals[1], 16 )
+
+                # Update amplitude
+                amplitude = write[0] << 16
+                amplitude += write[1]
+
             print line,
 
     # Print the newly modified version to stdout
@@ -36,4 +51,3 @@ while True:
     file.close()
 
     time.sleep(1)
-
