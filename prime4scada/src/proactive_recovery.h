bb11ee4739ff27daf705ca597dc988e22260dff0
TrevorAron <taron1@jhu.edu>
2015-05-27 16:45:22 -0400

    Removed unnecessary crap

--- prime4scada/src/proactive_recovery.h
+++
@@ -1,8 +0,0 @@
-#ifndef PROACTIVE_RECOVERY_H
-#define PROACTIVE_RECOVERY_H
-
-void RECOVERY_Initialize_Data_Structure(void);
-void RECOVERY_Initialize_Catch_Up_Struct(void);
-void RECOVERY_Catch_Up_Periodically(int, void *);
-void RECOVERY_Dispatcher(signed_message *);
-#endif
